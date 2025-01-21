c3c23254706dcc0f731feb10fcdc16ab7fc5c1c7
chrom9103 <kdkd21347.tm@gmail.com>
2025-01-21 21:08:38 +0900

    add msgCnt_limit

--- msgCnt_limit.py
+++ msgCnt_limit.py
@@ -3,6 +3,7 @@ import asyncio
 from collections import defaultdict
 import os
 from dotenv import load_dotenv
+from datetime import datetime, timedelta, timezone
 
 # Botのトークンを環境変数から取得
 load_dotenv('.env')
@@ -27,24 +28,33 @@ async def fetch_message_counts():
 
         print(f"Fetching message counts for guild: {guild.name}")
         user_message_count = defaultdict(int)  # 各ユーザーの発言数を記録
+        
+        jst = timezone(timedelta(hours=9))
+        start_date = datetime(2024, 4, 1, tzinfo=jst)
 
-        # サーバー内の全テキストチャンネルを処理
         for channel in guild.text_channels:
             print(f"Processing channel: {channel.name}")
             try:
-                # チャンネルのメッセージをカウント
                 async for message in channel.history(limit=None):
-                    user_message_count[message.author] += 1
-
-                for thread in channel.threads:
-                    print(f"  Processing thread: {thread.name}")
-                    async for message in thread.history(limit=None):
+                    if message.created_at.astimezone(jst) >= start_date:
                         user_message_count[message.author] += 1
-
             except discord.Forbidden:
                 print(f"Permission denied for channel: {channel.name}")
             except Exception as e:
                 print(f"Error processing channel {channel.name}: {e}")
+
+        for thread in guild.threads:
+            print(f"Processing thread: {thread.name}")
+            try:
+                async for message in thread.history(limit=None):
+                    if message.created_at.astimezone(jst) >= start_date:
+                        user_message_count[message.author] += 1
+
+            except discord.Forbidden:
+                print(f"Permission denied for Thread: {channel.name}")
+            except Exception as e:
+                print(f"Error processing thread {channel.name}: {e}")
+
 
         # サーバー全体の発言数を出力
         print("Total message counts per user:")
