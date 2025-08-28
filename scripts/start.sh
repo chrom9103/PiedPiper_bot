82933e41e64b6ab85a603c234d73dd82ead05434
chrom9103 <kdkd21347.tm@gmail.com>
2025-08-29 01:53:58 +0900

    fix

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,11 +1,5 @@
 #!/bin/bash
 
-# バックグラウンドで各Pythonスクリプトを起動
-python dice.py &
-python monitorVC.py &
-python morce_cat.py &
-python pin.py &
-python word_game.py &
+set -e
 
-# すべてのバックグラウンドプロセスが終了するのを待つ
-wait -n
+exec python -u src/main.py
